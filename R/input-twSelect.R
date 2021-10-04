e2ff15ea794ae45ffdb0ca061541098c4fcd443d
Kyle F. Butts <buttskyle96@gmail.com>
2021-10-04 09:16:29 -0500

    Started on theming shiny inputs

---
+++ R/input-twSelect.R
@@ -0,0 +1,56 @@
+#' Wrapper around shiny::selectInput but allowing for more classes
+#'
+#' Note selectize is turned off (for now)
+#'
+#' @export
+twSelectInput <- function(inputId,
+						label = NULL, choices, selected = NULL, multiple = FALSE,
+						container_class = NULL, label_class = NULL, select_class = NULL) {
+
+	if (!is.null(select_class)) {
+		select_class <- paste0("block form-control ", select_class, sep = " ")
+	} else {
+		select_class <- "block form-control"
+	}
+
+	if (!is.null(container_class)) {
+		container_class <- paste0("block twSelectInput form-group ", container_class, sep = " ")
+	} else {
+		container_class <- "block twSelectInput form-group"
+	}
+
+	if (!is.null(label_class)) {
+		label_class <- paste0("control-label ", label_class, sep = " ")
+	} else {
+		label_class <- "control-label"
+	}
+
+	if (!is.null(label)) {
+		label_tag <- tags$label(
+				class = label_class, id = paste0(inputId,"-label"),
+				`for` = inputId, label
+			)
+	} else {
+		label_tag <- NULL
+	}
+
+
+	# Extract options from select (IMO easier than rewriting)
+	temp <- shiny::selectInput(inputId = inputId, label = NULL, choices = choices, selectize = FALSE)
+
+	options <- shiny::HTML(
+		as.character(rvest::html_nodes(rvest::read_html(toString(temp)), xpath = "//option"))
+	)
+
+	tagList(
+		tags$div(
+			class = container_class,
+			label_tag,
+			tags$select(
+				id = inputId,
+				class = select_class,
+				options
+			)
+		)
+	)
+}
