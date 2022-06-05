cfb5d970b13609bab5665c927b4fa6861b7e6c78
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-06-05 11:31:26 +0200

    add comments to the 03-CSS_generation example and style it more obvious

--- inst/examples/03-CSS_generation/app.R
+++ inst/examples/03-CSS_generation/app.R
@@ -1,30 +1,53 @@
+###################################################
+#' This example app shows how the TailwindCSS CLI can be used
+#'
+#' The basic idea of the CLI is that the CSS classes are not fetched live from a
+#' CDN (an online source) and thus has a dependency on another online source.
+#' Instead the CLI looks at the code, extracts TailwindCSS classes that are used
+#' and compiles them to a local file, thus removing the online dependency.
+#'
+#' This requires the CLI (~35MB) to be installed locally, see also
+#' ?install_tailwindcss_cli
+#'
+#' See also: ?compile_tailwindcss, ?install_tailwindcss_cli and
+#' https://tailwindcss.com/blog/standalone-cli
+#'
+###################################################
+
 library(shiny)
 library(shiny.tailwind)
 
-# Install tailwindcss if needed
+# 1) Install tailwindcss if needed ----
 if (!is_tailwindcss_installed()) {
   install_tailwindcss_cli()
-  system("chmod +x tailwindcss")
+  if (Sys.info()[["sysname"]] != "Windows")
+    system("chmod +x tailwindcss")
 }
 
+# 2) Compile the used Tailwind CSS rules to a specific CSS file ----
+# This makes the CSS rules available for offline use
 # css goes into "www/" folder in shiny apps
-compile_tailwindcss(
+compiled_css_file <- compile_tailwindcss(
   infile = "custom.css", outfile = "www/tailwind-out.css",
-  verbose = T
+  verbose = TRUE
 )
 
+# 3) Define UI for the shiny app, including the compiled Tailwind CSS ----
 ui <- div(
   class = "page-div",
-  # tailwindcss_compile returns the outfile silently, therefore this should work
+  # Adds the compiled CSS files to the app
   tags$head(
     tags$link(
       rel = "stylesheet", type = "text/css",
-      href = "tailwind-out.css" # no www/ here
+      href = gsub("^www/", "", compiled_css_file) # no www/ here
     ),
   ),
+  # add some Tailwind CSS classes here
   div(
-    class = "w-full text-center py-12",
-    h1("Hello World")
+    class = "w-full text-center py-12 flex justify-center",
+    div(class = "p-4 max-w-xl bg-orange-400 hover:bg-orange-600 rounded text-2xl font-serif font-normal hover:font-bold",
+        "Hello World"
+    )
   )
 )
 
@@ -32,5 +55,5 @@ server <- function(input, output) {
 
 }
 
-# Run the application
+# 4) Run the application ----
 shinyApp(ui = ui, server = server)
