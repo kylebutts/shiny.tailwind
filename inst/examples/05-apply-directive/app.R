e95d1e67b4cd9a3acf3b3fc43e8e88f43ddb72e8
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-06-12 11:19:44 +0200

    add styled selectize background to example

--- inst/examples/05-apply-directive/app.R
+++ inst/examples/05-apply-directive/app.R
@@ -3,13 +3,21 @@
 #' inside a shiny application using shiny.tailwind.
 #'
 #' We define new classes in the css file and reference them here, eg: main-div,
-#' fancy-title, and rounded-box.
+#' fancy-title, rounded-box as well as the the background of the twSelectInput
+#' elements (these elements are styled by selectize.bootstrap.css but are
+#' overridden using the @apply directive.
 #'
 #' See also: https://tailwindcss.com/docs/reusing-styles#extracting-classes-with-apply
 ################################################################################
 
 library(shiny)
 library(shiny.tailwind)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+theme_set(theme_light())
+
+dataset <- mpg %>% select_if(is.numeric)
 
 # Define UI for application that draws a histogram
 ui <- div(
@@ -19,21 +27,26 @@ ui <- div(
 
   # Title
   div(class = "flex flex-col w-full text-center py-12",
-      h1(class = "fancy-title", "Old Faithful")
+      h1(class = "fancy-title", "Histogram of Cars")
   ),
 
   # Inputs
   div(class = "rounded-box block py-4 px-4 flex flex-row",
+
+      div(class = "rounded-box flex-initial mx-4",
+          # note the background of the inputs is styled in apply-custom.css
+          twVarSelectInput("var", "Variable:", dataset, multiple = TRUE,
+                           container_class = "mx-2 w-64"
+                           ),
+      ),
       div(class = "rounded-box flex-initial mx-4",
           sliderInput("bins", "Number of Bins:",
                       min = 1, max = 10, value = 5)
       ),
       div(class = "rounded-box flex-initial mx-4",
-          textInput("firstname", "First Name", value = "")
-      ),
-      div(class = "rounded-box flex-initial mx-4",
-          textInput("lastname", "Last Name", value = "")
-      ),
+          twTextInput("title", "Title", value = "Histogram of Cars",
+                      input_class = "rounded-md text-teal-500 font-bold")
+      )
   ),
 
   # Plot
@@ -44,16 +57,14 @@ ui <- div(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
   output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x,
-         breaks = bins,
-         col = 'darkgray', border = 'white')
+    dataset %>%
+      select(!!!input$var) %>%
+      pivot_longer(everything()) %>% # TODO replace with reshape to reduce dependencies
+      ggplot(aes(x = value, fill = name)) +
+      geom_histogram(bins = input$bins + 1) +
+      facet_wrap(~name, scales = "free_x") +
+      labs(title = input$title)
   })
 }
 
