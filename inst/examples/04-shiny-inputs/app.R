9c52bcd67ed8d27212e7994a852366f5ebaf476c
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-06-28 13:10:41 +0200

    make selectize = TRUE the default again

--- inst/examples/04-shiny-inputs/app.R
+++ inst/examples/04-shiny-inputs/app.R
@@ -38,7 +38,7 @@ form1 <- rounded_container(
   twSelectInput(
     inputId = "countries",label = "Select your country",
     choices = c("United States", "Canada", "France", "Germany"),
-    container_class = "mt-6", selectize = FALSE,
+    container_class = "mt-6", selectize = TRUE,
     label_class = "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400",
     select_class = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
   ),
