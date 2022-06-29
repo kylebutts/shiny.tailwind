48b42b400e92127783d9523a5016d58cd867624b
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-06-29 11:24:11 +0200

    update daisyui example

--- inst/examples/07-daisyUI/app.R
+++ inst/examples/07-daisyUI/app.R
@@ -14,7 +14,7 @@ library(shiny.tailwind)
 # small helper to create a pre.code tag.
 # Note that shiny::tags$pre + shiny::tags$code create new lines which break the
 # way the code will be displayed
-code <- function(code, prefix = "$", class = NULL) {
+code_mockup <- function(code, prefix = "$", class = NULL) {
   shiny::HTML(sprintf(
     '<pre data-prefix="%s"%s><code>%s</code></pre>',
     prefix,
@@ -25,56 +25,90 @@ code <- function(code, prefix = "$", class = NULL) {
   ))
 }
 
+# title element similar to h1()
+title <- function(txt) div(txt, class = "text-4xl font-bold mb-2")
+
+# styled element for more info
+see_also <- function(txt, url, class = "") {
+  div(class = paste("text-gray-500", class),
+      txt,
+      a(href = url, url,
+        target = "_blank",
+        class = "text-blue-600 hover:underline decoration-blue-600")
+  )
+}
+
 
 ui <- div(
-  class = "grid grid-cols-4 gap-4 p-2",
   use_daisyui(),
 
   div(
-    div("Breadcrumbs", class = "text-2xl font-bold"),
+    class = "p-2 mb-4",
+    div("daisyUI Examples", class = "text-center text-6xl font-bold mb-2"),
+    see_also("More information about daisyUI can be found here:",
+             "https://daisyui.com/", class = "text-center text-xl")
+  ),
+
+  div(
+    class = "grid grid-cols-4 gap-4 p-2",
+
     div(
-      class = "text-sm breadcrumbs",
-      tags$ul(
-        tags$li(tags$a("Home")),
-        tags$li(tags$a("Documents")),
-        tags$li(tags$a("Add Documents")),
+      title("Breadcrumbs"),
+      see_also("See also: ", "https://daisyui.com/components/breadcrumbs/",
+               class = "mb-2"),
+
+      div(
+        class = "text-sm breadcrumbs",
+        tags$ul(
+          tags$li(icon("home"), a("Home", class = "ml-1")),
+          tags$li(icon("folder"), a("Documents", class = "ml-1")),
+          tags$li(icon("folder-plus"), a("Add Documents", class = "ml-1")),
+        )
       )
-    )
-  ),
+    ),
 
-  div(
-    div("Steps", class = "text-2xl font-bold"),
-    tags$ul(
-      class = "steps",
-      tags$li(class = "step step-primary", "Learn R"),
-      tags$li(class = "step step-primary", "Make Shiny App"),
-      tags$li(class = "step", "???"),
-      tags$li(class = "step", "Profit!!!"),
-    )
-  ),
-
-  div(
-    div("Pagination", class = "text-2xl font-bold"),
     div(
-      class = "btn-group",
-      tags$button(class = "btn btn-md", 1),
-      tags$button(class = "btn btn-md", 2),
-      tags$button(class = "btn btn-md btn-active", 3),
-      tags$button(class = "btn btn-md", 4),
-    )
-  ),
+      title("Steps"),
+      see_also("See also: ", "https://daisyui.com/components/steps/",
+               class = "mb-2"),
 
-  div(
-    class = "w-96",
-    div("Code", class = "text-2xl font-bold"),
+      tags$ul(
+        class = "steps",
+        tags$li(class = "step step-primary", "Learn R"),
+        tags$li(class = "step step-primary", "Make Shiny App"),
+        tags$li(class = "step", "???"),
+        tags$li(class = "step", "Profit!!!"),
+      )
+    ),
+
     div(
-      class="mockup-code",
-      # Note that tags$pre + tags$code introduces newlines which breaks the code
-      code("R"),
-      code("install.packages(\"shiny.tailwind\")", prefix = "R", class = "text-warning"),
-      code("install_tailwindcss_ui()", prefix = "R", class = "text-success")
+      title("Pagination"),
+      see_also("See also: ", "https://daisyui.com/components/pagination/",
+               class = "mb-2"),
+      div(
+        class = "btn-group",
+        tags$button(class = "btn btn-md", 1),
+        tags$button(class = "btn btn-md", 2),
+        tags$button(class = "btn btn-md btn-active", 3),
+        tags$button(class = "btn btn-md", 4),
+      )
+    ),
+
+    div(
+      # class = "w-96",
+      title("Code"),
+      see_also("See also: ", "https://daisyui.com/components/code/"),
+
+      div(
+        class="mockup-code",
+        # Note that tags$pre + tags$code introduces newlines which breaks the code
+        code_mockup("R"),
+        code_mockup("install.packages(\"shiny.tailwind\")", prefix = "R",
+                    class = "text-warning"),
+        code_mockup("install_tailwindcss_ui()", prefix = "R",
+                    class = "text-success")
+      )
     )
   )
 )
-
 shinyApp(ui, function(input, output) {})
