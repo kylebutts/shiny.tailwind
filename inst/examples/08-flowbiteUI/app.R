fec2e6d25aaf44f057d5d523390085279377de27
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-06-29 11:23:46 +0200

    update flowbite example

--- inst/examples/08-flowbiteUI/app.R
+++ inst/examples/08-flowbiteUI/app.R
@@ -11,113 +11,209 @@
 library(shiny)
 library(shiny.tailwind)
 
+# 1) Define Helper Functions ====
 
+# title element similar to h1()
+title <- function(txt) div(txt, class = "text-4xl font-bold mb-2")
+
+# styled element for more info
+see_also <- function(txt, url, class = "") {
+  div(class = paste("text-gray-500", class),
+      txt,
+      a(href = url, url,
+        target = "_blank",
+        class = "text-blue-600 hover:underline decoration-blue-600")
+  )
+}
+
+# creates a button with a tooltip
+tooltip_button <- function(id, label, tooltip_text,
+                           tooltip_position = c("top", "right", "bottom", "left")) {
+  tooltip_position <- match.arg(tooltip_position)
+
+  tagList(
+    # the button element
+    tags$button(
+      "data-tooltip-target" = paste0(id, "-tooltip"),
+      "data-tooltip-placement" = tooltip_position,
+      type = "button",
+      class = paste(
+        "mb-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4",
+        "focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm",
+        "px-5 py-2.5 text-center"),
+      label),
+
+    # the tooltip
+    div(
+      id = paste0(id, "-tooltip"), role = "tooltip",
+      class = paste(
+        "inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium",
+        "text-white bg-gray-900 rounded-lg shadow-sm opacity-0",
+        "transition-opacity duration-300 tooltip"),
+      tooltip_text,
+      div(class = "tooltip-arrow", "data-popper-arrow" = NA))
+  )
+}
+
+# creates the HTML code for the accordion
+flowbite_accordion <- function(id, ...) {
+  ll <- list(...)
+
+  svg_path <- paste(
+    "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4",
+    "4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+  )
+  svg_down <- tags$svg("data-accordion-icon" = NA, class = "w-6 h-6 shrink-0",
+                       fill = "currentColor", viewBox = "0 0 20 20",
+                       xmlns = "http://www.w3.org/2000/svg",
+                       tags$path("fill-rule" = "evenodd", d = svg_path,
+                                 "clip-rule" = "evenodd"))
+
+  div(id = id, "data-accordion" = "collapse",
+      "data-active-classes" = "bg-blue-100 text-blue-600",
+      lapply(seq_along(ll), function(i) {
+        tagList(
+          h2(id = paste0(id, "-heading-", i),
+             tags$button(
+               type = "button",
+               class = paste(
+                 "flex justify-between items-center p-5 w-full font-medium",
+                 "text-left text-gray-500 border border-b-0",
+                 "border-gray-200 focus:ring-4 focus:ring-gray-200",
+                 "hover:bg-gray-100", if (i == 1) "rounded-t-xl"),
+               "data-accordion-target" = paste0("#", id, "-body-", i),
+               "aria-expanded" = "true",
+               "aria-controls" = paste0(id, "-body-", i),
+               span(names(ll)[[i]]),
+               svg_down
+             )
+          ),
+          div(
+            id = paste0(id, "-body-", i), class = "hidden",
+            "aria-labelledby" = paste0(id, "-heading-", i),
+            div(
+              class = "p-5 border border-b-0 border-gray-200",
+              ll[[i]]
+            )
+          )
+        )
+      })
+  )
+}
+# define the svg code for a paperplane -> simple toast element
+svg_plane <- paste(
+  "M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27",
+  "4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86",
+  "76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192",
+  "491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69",
+  "317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72",
+  "16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"
+)
+
+# Define the App =======
 ui <- div(
-  class = "grid grid-rows-3 gap-4 mb-6 p-2",
-
   use_flowbite(),
 
+  # Header element :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   div(
-    div("Tabs", class = "text-2xl font-bold"),
-
-    HTML('
-         <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-    <li class="mr-2">
-        <a href="#" aria-current="page" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Profile</a>
-    </li>
-    <li class="mr-2">
-        <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</a>
-    </li>
-    <li class="mr-2">
-        <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</a>
-    </li>
-    <li class="mr-2">
-        <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</a>
-    </li>
-    <li>
-        <a class="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
-    </li>
-</ul>
-         ')
+    class = "p-2 mb-4",
+    div("Flowbite UI Examples", class = "text-center text-6xl font-bold mb-2"),
+    see_also("More information about flowbite can be found here:",
+             "https://flowbite.com/", class = "text-center text-xl")
   ),
 
+  # Example elements :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   div(
-    div("Input Validation", class = "text-2xl font-bold"),
+    class = "grid grid-cols-3 gap-4 mx-2",
 
-    HTML('
-       <div class="mb-6">
-  <label for="success" class="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">Your name</label>
-  <input type="text" id="success" class="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="Success input">
-  <p class="mt-2 text-sm text-green-600 dark:text-green-500"><span class="font-medium">Well done!</span> Some success messsage.</p>
-</div>
-<div>
-  <label for="error" class="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">Your name</label>
-  <input type="text" id="error" class="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500" placeholder="Error input">
-  <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> Some error message.</p>
-</div>
-       ')
+    # Tooltips :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    div(
+      title("Tooltips"),
+      see_also("See also:", "https://flowbite.com/docs/components/tooltips/",
+               class = "mb-2"),
+
+      div(class = "flex justify-start gap-2",
+          tooltip_button("btn1", "Above me", "Now I am visible", "top"),
+          tooltip_button("btn2", "To the right", "Now I am visible", "right"),
+          tooltip_button("btn3", "Below me", "Now I am visible", "bottom"),
+          tooltip_button("btn4", "To the left", "Now I am visible", "left")
+      ),
     ),
 
 
+    # Accordion ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    div(
+      title("Accordion"),
+      see_also("See also:", "https://flowbite.com/docs/components/accordion/",
+               class = "mb-2"),
 
+      flowbite_accordion(
+        id = "accordion-open",
+        "What is Flowbite?" = list(
+          p(class = "mb-2 text-gray-500", paste(
+            "Flowbite is an open-source library of interactive components built",
+            "on top of Tailwind CSS including buttons, dropdowns, modals,",
+            "navbars, and more."
+          )),
+          p(class = "text-gray-500", HTML(paste(
+            "Check out this guide to learn how to ",
+            "<a href=\"https://flowbite.com/docs/getting-started/introduction/\"",
+            "class=\"text-blue-600 dark:text-blue-500 hover:underline\">",
+            "get started</a> and start developing websites even faster with ",
+            "components on top of Tailwind CSS."
+          )))
+        ),
+        "What are the differences between Flowbite and Tailwind UI?" = list(
+          p(class = "mb-2 text-gray-500", paste(
+            "The main difference is that the core components from Flowbite are",
+            "open source under the MIT license, whereas Tailwind UI is a paid",
+            "product. Another difference is that Flowbite relies on smaller and",
+            "standalone components, whereas Tailwind UI offers sections of pages."
+          )
+          ),
+          p(class = "text-gray-500", paste(
+            "However, we actually recommend using both Flowbite, Flowbite Pro,",
+            "and even Tailwind UI as there is no technical reason stopping you",
+            "from using the best of two worlds."
+          )),
+          p(class = "text-gray-500", "Learn more about these technologies:"),
+          tags$ul(
+            class = "pl-5 list-disc text-gray-500 dark:text-gray-400",
+            tags$li(a(href = "https://flowbite.com/pro/",
+                      class = "text-blue-600 hover:underline",
+                      "Flowbite Pro")),
+            tags$li(a(href = "https://tailwindui.com/",
+                      class = "text-blue-600 hover:underline",
+                      "Tailwind UI"
+            )))
+        )
+      )
+    ),
 
-  div(
-    div("Sidebar", class = "text-2xl font-bold"),
+    # Toast ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    div(
+      title("Simple Toast"),
+      see_also("See also:", "https://flowbite.com/docs/components/toast/",
+               class = "mb-2"),
 
-#     HTML('
-#          <div class="w-64" aria-label="Sidebar">
-#    <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
-#       <ul class="space-y-2">
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-#                <span class="ml-3">Dashboard</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-#                <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-#                <span class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-#             </a>
-#          </li>
-#          <li>
-#             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-#                <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-#                <span class="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-#             </a>
-#          </li>
-#       </ul>
-#    </div>
-# </div>')
+      div(
+        id = "toast-simple",
+        class = paste(
+          "flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white",
+          "divide-x divide-gray-200 rounded-lg shadow space-x"
+        ),
+        role = "alert",
+        tags$svg(
+          class = "w-5 h-5 text-blue-600", "aria-hidden" = "true",
+          focusable = "false", "data-prefix" = "fas",
+          "data-icon" = "paper-plane", "role" = "img",
+          xmlns = "http://www.w3.org/2000/svg", viewBox = "0 0 512 512",
+          tags$path(fill = "currentColor", d = svg_plane)
+        ),
+        div(class = "pl-4 text-sm font-normal", "Message sent successfully")
+      )
+    )
   )
 )
-
-
 
 shinyApp(ui, function(input, output) {})
