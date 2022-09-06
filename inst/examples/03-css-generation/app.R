ad86c73076268a115ab74d85394199e9f50f60c9
David Zimmermann-Kollenda <david-zimmermann@hotmail.de>
2022-09-06 23:38:09 +0200

    run css-generation example only in interactive session (downloads CLI)

--- inst/examples/03-css-generation/app.R
+++ inst/examples/03-css-generation/app.R
@@ -17,44 +17,48 @@
 library(shiny)
 library(shiny.tailwind)
 
-# 1) Install tailwindcss if needed ----
-if(!is_tailwindcss_installed()) {
-  install_tailwindcss_cli()
-  if(Sys.info()[["sysname"]] != "Windows")
-    system("chmod +x tailwindcss")
-}
+if (interactive()) {
+
+  # 1) Install tailwindcss if needed ----
+
+  if(!is_tailwindcss_installed()) {
+    install_tailwindcss_cli()
+    if(Sys.info()[["sysname"]] != "Windows")
+      system("chmod +x tailwindcss")
+  }
 
 
-# 3) Define UI for the shiny app, including the compiled Tailwind CSS ----
-ui <- div(
-  class = "page-div",
-  # 2) Compile the used Tailwind CSS rules to a specific CSS file ----
-  # This makes the CSS rules available for offline use
-  # css goes into "www/" folder in shiny apps
-  # Runs each refresh
-  compile_tailwindcss(
-    infile = "custom.css", outfile = "www/tailwind-out.css",
-    verbose = TRUE
-  ),
-  # Adds the compiled CSS files to the app
-  tags$head(
-    tags$link(
-      rel = "stylesheet", type = "text/css",
-      href = "tailwind-out.css" # no www/ here
+  # 3) Define UI for the shiny app, including the compiled Tailwind CSS ----
+  ui <- div(
+    class = "page-div",
+    # 2) Compile the used Tailwind CSS rules to a specific CSS file ----
+    # This makes the CSS rules available for offline use
+    # css goes into "www/" folder in shiny apps
+    # Runs each refresh
+    compile_tailwindcss(
+      infile = "custom.css", outfile = "www/tailwind-out.css",
+      verbose = TRUE
     ),
-  ),
-  # add some Tailwind CSS classes here
-  div(
-    class = "w-full text-center py-12 flex justify-center",
-    div(class = "p-4 max-w-xl bg-violet-400 border-2 border-violet-300 hover:shadow-lg rounded text-2xl font-serif font-normal text-white",
-        "Hello World"
+    # Adds the compiled CSS files to the app
+    tags$head(
+      tags$link(
+        rel = "stylesheet", type = "text/css",
+        href = "tailwind-out.css" # no www/ here
+      ),
+    ),
+    # add some Tailwind CSS classes here
+    div(
+      class = "w-full text-center py-12 flex justify-center",
+      div(class = "p-4 max-w-xl bg-violet-400 border-2 border-violet-300 hover:shadow-lg rounded text-2xl font-serif font-normal text-white",
+          "Hello World"
+      )
     )
   )
-)
 
-server <- function(input, output) {
+  server <- function(input, output) {
 
+  }
+
+  # 4) Run the application ----
+  shiny::shinyApp(ui = ui, server = server)
 }
-
-# 4) Run the application ----
-shiny::shinyApp(ui = ui, server = server)
